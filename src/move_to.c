33f65b4283e6ec60a3fc6f4decafb82700c1ec7b
Nicolas Constanty <nicolas.constanty@epitech.eu>
2016-03-26 15:45:03 +0100

    norme

--- src/move_to.c
+++ src/move_to.c
@@ -5,51 +5,10 @@
 ** Login	wery_a
 **
 ** Started on	Wed Mar 23 11:31:53 2016 Adrien WERY
-** Last update	Thu Mar 24 23:14:00 2016 Nicolas Constanty
+** Last update	Sat Mar 26 13:59:40 2016 Nicolas Constanty
 */
 
 #include "lemipc.h"
-
-int	moveMsg(t_player *p)
-{
-  t_msg    msgR;
-  t_msg    msgS;
-  static unsigned int i = 0;
-
-  if (msgrcv(p->msgID, &msgR, sizeof(t_msg), p->nteam, MSG_NOERROR |
-  IPC_NOWAIT) == -1)
-  {
-    msgS.mtype = p->nteam;
-    msgS.pos = getPos(p->y, p->x);
-    if (msgsnd(p->msgID, &msgS, sizeof(t_msg), 0) == -1)
-    perror("msgsnd");
-    printf("%s\n", "coucou");
-    p->caps = true;
-    return (attack(p, 3));
-  }
-  ++i;
-  if (i < 50 || rand() % 3 == 0)
-  {
-    printf("%d\n", p->caps);
-    if (p->caps == false && msgsnd(p->msgID, &msgR, sizeof(t_msg), 0) == -1)
-    perror("msgsnd");
-    return (msgR.pos);
-  }
-  if (checkChance(p, 3, 0, 0) > 0)
-  {
-    msgS.pos = avoid(p);
-    if (p->caps == false && msgsnd(p->msgID, &msgS, sizeof(t_msg), 0) == -1)
-    perror("msgsnd");
-    return (msgS.pos);
-  }
-  else
-  {
-    if (p->caps == false && msgsnd(p->msgID,
-      &msgS, sizeof(t_msg), 0) == -1)
-      perror("msgsnd");
-      return (attack(p, 3));
-  }
-}
 
 void	moveToY(t_player *p, int y, int dice)
 {
